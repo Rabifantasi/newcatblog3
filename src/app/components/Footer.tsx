1155fcfe7ef037f14c9bb784d6af733c7817738a
Rabifantasi <159634085+Rabifantasi@users.noreply.github.com>
2024-12-29 20:44:22 +0500

    changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,20 @@
 export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white p-4 mt-8">
-        <div className="container mx-auto text-center">
+  return (
+    <footer className="bg-gray-800 text-white p-10 mt-8">
+      <div className="container mx-auto text-center">
+        <div className="mb-8">
+          <h3 className="text-lg font-bold mb-4">Connect with Us</h3>
+          <p className="text-sm">
+            We love hearing from our community! Feel free to reach out with your thoughts and suggestions.
+          </p>
+        </div>
+        <div className="mt-8">
           <p>&copy; {new Date().getFullYear()} All About Cat. All Rights Reserved.</p>
           <p>
             <span className="text-red-500">&hearts;</span> for cat lovers.
           </p>
         </div>
-      </footer>
-    );
-  }
+      </div>
+    </footer>
+  );
+}
