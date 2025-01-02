5792fed564477c2f882b96094f31eee5239260d7
Rabifantasi <159634085+Rabifantasi@users.noreply.github.com>
2025-01-02 16:07:00 +0500

    changes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,14 +7,14 @@ export default function Header() {
         <Link href="/" className="text-2xl font-bold text-white mb-2 sm:mb-0">
           All About Cats
         </Link>
-        <nav className="flex flex-col sm:flex-row">
-          <Link href="/" className="mx-2 text-black hover:text-blue-800 text-base sm:text-lg">
+        <nav className="flex flex-col sm:flex-row sm:space-x-4">
+          <Link href="/" className="text-black hover:text-blue-800 text-base sm:text-lg mb-2 sm:mb-0">
             Home
           </Link>
-          <Link href="/about" className="mx-2 text-black hover:text-blue-800 text-base sm:text-lg">
+          <Link href="/about" className="text-black hover:text-blue-800 text-base sm:text-lg mb-2 sm:mb-0">
             About
           </Link>
-          <Link href="/contact" className="mx-2 text-black hover:text-blue-800 text-base sm:text-lg">
+          <Link href="/contact" className="text-black hover:text-blue-800 text-base sm:text-lg mb-2 sm:mb-0">
             Contact
           </Link>
         </nav>
